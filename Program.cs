162b28df2dbde434324553c2eea28f8e13a75a06
Martin Hesdahl <Martin@watch-e.nl>
2022-12-20 22:16:14 +0100

    Day1Part1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿var path = "CaloriesInput.txt";
+var input = await File.ReadAllLinesAsync(path);
+
+var elfsCalories = new List<int>();
+
+int caloriesForOneElf = 0;
+
+foreach (var line in input)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        elfsCalories.Add(caloriesForOneElf);
+        caloriesForOneElf = 0;
+        continue;
+    }
+
+    caloriesForOneElf += int.Parse(line);
+}
+
+System.Console.WriteLine(elfsCalories.Max());
+Console.ReadLine();
